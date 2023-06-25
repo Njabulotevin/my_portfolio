e12c64d8e2a1299aaea3986f4486d7bc935f16a6
ntevin25@gmail.com <ntevin25@gmail.com>
2023-06-25 17:10:21 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,208 @@
-import Image from 'next/image'
+import { Github, Mail, Phone } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const skills = [
+    { label: "Software Development", icon: "/Vector(1).png" },
+    { label: "Web Development", icon: "/Vector(2).png" },
+    { label: "UI/UX Design", icon: "/Vector(3).png" },
+  ];
+
+  const tech = [
+    "Typescript",
+    "Javascript",
+    "Dart",
+    "Python",
+    "HTML",
+    "Sass",
+    "TailwindCss",
+    "NextJs",
+    "ReactJs",
+  ];
+
+  const contacts = [
+    { label: "Email Address", icon: <Mail />, value: "ntevin25@gmail.com" },
+    { label: "Github", icon: <Github />, value: "@njabulotevin" },
+    { label: "Phone Number", icon: <Phone />, value: "(+72) 64 891 7936" },
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="overflow-x-hidden">
+      <main className="lg:p-20 lg:pt-[50px]">
+        <section className="flex justify-between max-w-[70vw] mx-auto">
+          <img
+            src="/Tev..png"
+            alt="logo"
+            className="w-[40px] h-[100px] lg:h-[54px] lg:w-[100px] object-contain"
+          />
+          <div className="hidden lg:flex">
+            <ul className="flex gap-10 font-medium text-lg text-gray-100">
+              <li className="cursor-pointer">About Me</li>
+              <li className="cursor-pointer">Project</li>
+              <li className="cursor-pointer">Contact</li>
+            </ul>
+          </div>
+        </section>
+        <section
+          style={{
+            backgroundSize: "100%",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+          className="bg-[url('/developer.png')] flex flex-wrap flex-col w-full min-h-[80vh] px-10 pt-20 py-10"
+        >
+          <div className="flex flex-wrap gap-24 justify-center items-center text-gray-100">
+            <div className="flex flex-col gap-4">
+              <h1 className="text-4xl lg:text-8xl flex flex-col  font-semibold">
+                <span>Hey There,</span>{" "}
+                <span>
+                  I&apos;m <span className="text-secondary-900">Tevin</span>
+                </span>
+              </h1>
+              <div className="max-w-[400px] flex flex-col gap-4">
+                <p>
+                  A frontend developer with a passion for creating visually
+                  appealing and user-friendly web experiences.
+                </p>
+                <p>
+                  With expertise in frontend technologies such as Next.js, Sass,
+                  Tailwind CSS, and TypeScript,
+                </p>
+              </div>
+            </div>
+            <div className="w-[300px] lg:w-[600px] lg:h-[600px]">
+              <img src="/me_1.png" alt="" />
+            </div>
+          </div>
+          <div>
+            <img className="mx-auto" src="/tech.png" />
+          </div>
+        </section>
+      </main>
+      <section className="flex gap-5 flex-wrap justify-center lg:gap-3 bg-white min-h-[60vh] px-10 lg:px-20 py-36">
+        <div className="flex flex-col gap-3 ">
+          <h2 className="text-6xl lg:text-8xl font-semibold text-secondary-900">
+            5+
+          </h2>
+          <h3 className="text-lg lg:text-3xl font-semibold text-primary-900 max-w-[220px]">
+            /Years of working Experience
+          </h3>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <div className="flex flex-col gap-4 lg:max-w-[48vw] my-10 lg:m-0">
+          <h2 className="text-4xl font-semibold text-secondary-900">
+            About me
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <p className="font-semibold text-primary-900">
+            I&apos;m a frontend developer with a passion for creating visually
+            appealing and user-friendly web experiences. With expertise in
+            frontend technologies such as{" "}
+            <span className="text-secondary-900">
+              Next.js, Sass, Tailwind CSS, Javascript and TypeScript
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            , I specialize in designing and building robust and responsive
+            frontend projects.{" "}
           </p>
-        </a>
+          <p className=" text-primary-900">
+            I thrive on crafting elegant and efficient code that brings ideas to
+            life and ensures an exceptional user experience. From
+            conceptualization to implementation, I enjoy collaborating with
+            teams to create innovative solutions that exceed expectations. With
+            a keen eye for detail and a commitment to staying updated on the
+            latest industry trends, I am dedicated to delivering high-quality
+            work that aligns with both user needs and business goals. Let&apos;s
+            collaborate to bring your frontend projects to the next level!
+          </p>
+        </div>
+      </section>
+      <section
+        style={{
+          backgroundSize: "100%",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "100% 40%",
+        }}
+        className="px-20 flex flex-col items-center gap-10 py-36 min-h-[60vh] bg-[url('/developer.png')]"
+      >
+        <h2 className="text-2xl lg:text-4xl font-semibold text-white text-center">
+          What I do
+        </h2>
+        <div className="flex justify-center gap-10 flex-wrap px-10 lg:p-0">
+          {skills.map((skill, i) => {
+            return (
+              <div
+                key={i}
+                className="bg-primary-500 w-full lg:w-[350px] h-[180px] lg:h-[240px] px-[50px] py-[20px] flex flex-col gap-5 rounded-tl-[20px] rounded-br-[20px]"
+              >
+                <img
+                  src={skill.icon}
+                  className="w-[30px] h-[30px] lg:w-[50px] lg:h-[50px] object-contain"
+                />
+                <h4 className="text-primary-300 font-semibold text-2xl lg:text-3xl">
+                  {skill.label}
+                </h4>
+              </div>
+            );
+          })}
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <div className="flex flex-wrap gap-10 justify-center">
+          {tech.map((tech, i) => {
+            return (
+              <div
+                key={i}
+                className="text-white border border-white rounded-md px-3 py-2 font-semibold"
+              >
+                {tech}
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <section className="lg:px-20 p-10 lg:py-36 flex flex-wrap justify-center gap-4 min-h-[60vh]">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-4xl font-semibold text-secondary-900">
+            My Recent Work
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+          <h3 className="text-3xl font-semibold text-white">Uzoori App</h3>
+          <p className=" text-primary-300 w-full lg:max-w-[30vw]">
+            Uzoori is a social commerce app that allows users to discover new
+            styles and trends in the beauty and food industries, book
+            appointments with stylists and make orders for food from local
+            restaurants. Users can also engage with other users and merchants,
+            follow their favorite stylists and restaurants, and even earn money
+            through an affiliate program. visit beta:
+            <Link
+              className="text-secondary-900 underline"
+              href="https://uzoori.app"
+            >
+              uzoori.app
+            </Link>{" "}
+            powered by Nextjs.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+        <div className="">
+          <img src="/uzoori.png" />
+        </div>
+      </section>
+      <section className="px-20 py-36 ">
+        <div className="flex flex-wrap justify-center gap-10 lg:gap-36">
+          {contacts.map((contact, i) => {
+            return (
+              <div className="flex flex-col gap-3 items-center text-white">
+                <div className="bg-primary-500 text-secondary-900 w-[70px] h-[70px] rounded-full flex justify-center items-center mb-8">
+                  {contact.icon}
+                </div>
+                <h4 className="font-semibold text-base">{contact.label}</h4>
+                <p className="text-sm">{contact.value}</p>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+      <footer>
+        <p className="text-primary-300 text-sm text-center py-10">
+          All copyright reserved © Tevin 2023
+        </p>
+      </footer>
+    </div>
+  );
 }
